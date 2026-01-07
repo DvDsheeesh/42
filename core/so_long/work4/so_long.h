bef1e54f3c6f24052068302f7d8228f8d0fc27bc
Mohammad Elshatarat <melshata@c1r4s4.42amman.com>
2026-01-07 12:05:05 +0300

    work4 but less than needed

---
+++ core/so_long/work4/so_long.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melshata <melshata@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/04 11:37:44 by melshata          #+#    #+#             */
+/*   Updated: 2026/01/04 11:52:27 by melshata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+
+# include <stdio.h>////////////////////
+# include <stdlib.h>
+# include <unistd.h>
+
+	typedef struct	s_point
+	{
+		int			x;
+		int			y;
+	}				t_point;
+
+#endif
