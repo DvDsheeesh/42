367c760e7bb5c410c495ab816c3950acf9d39540
Mohammad Elshatarat <melshata@c1r5s11.42amman.com>
2026-01-10 15:55:09 +0300

    all done, not checked malloc break

---
+++ core/so_long/work6/mapcheck/map_ch1.c
@@ -0,0 +1,356 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_ch1.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melshata <melshata@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/04 09:57:35 by melshata          #+#    #+#             */
+/*   Updated: 2026/01/10 15:16:19 by melshata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+char	**freemap(char **map, char **str)
+{
+	int	i;
+
+	i = 0;
+	while (map && map[i])
+		printf("%s\n", map[i++]);
+	printf("%d\n", i);
+	i = 0;
+	while (map && map[i])
+		free(map[i++]);
+	if (map)
+		free(map);
+	if (str && *str)
+	{
+		free(*str);
+		*str = NULL;
+	}
+	return (NULL);
+}
+
+char	*extend_str(char *s1, char *s2)
+{
+	char	*ss;
+	ssize_t	i;
+	ssize_t	j;
+
+	ss = malloc(ll(s1) + ll(s2) + 1);
+	if (!ss)
+		return (NULL);
+	i = 0;
+	if (s1)
+	{
+		while (s1[i])
+		{
+			ss[i] = s1[i];
+			i++;
+		}
+	}
+	free(s1);
+	j = 0;
+	if (s2)
+		while (s2[j])
+			ss[i++] = s2[j++];
+	free(s2);
+	ss[i] = '\0';
+	return (ss);
+}
+
+char	*read_file(int fd)
+{
+	char	*line;
+	char	*str;
+
+	str = NULL;
+	line = get_next_line(fd);
+	while (line)
+	{
+		str = extend_str(str, line);
+		line = get_next_line(fd);
+	}
+	if (line)
+		free(line);
+	return (str);
+}
+
+int	isvalid(char *c, int *ccount, int *pcount, int *ecount)
+{
+	if (*c == '0' || *c == '1'
+		|| *c == 'C' || *c == 'P' || *c == 'E'
+		|| (*(c) == '\n' && *(c + 1) != '\n' && *(c - 1) != '\n'))
+	{
+		if (*c == 'C')
+			(*ccount)++;
+		if (*c == 'P')
+			(*pcount)++;
+		if (*c == 'E')
+			(*ecount)++;
+		return (1);
+	}
+	return (0);
+}
+
+int	phasing(char **text)
+{
+	int	i;
+	int	ccount;
+	int	pcount;
+	int	ecount;
+
+	ccount = 0;
+	pcount = 0;
+	ecount = 0;
+	i = 0;
+	while ((*text)[i] && (*text)[i] == '\n')
+		i++;
+	while ((*text)[i] && isvalid(&((*text)[i]), &ccount, &pcount, &ecount))
+		i++;
+	while ((*text)[i] && (*text)[i] == '\n')
+		i++;
+	if ((*text)[i])
+		write (1, "Error\ninvalid character or map shape\n", 37);
+	else if (pcount != 1 || ecount != 1 || ccount < 1)
+		write (1, "Error\nwrong number of P, E, C\n", 30);
+	else
+		return (1);
+	return (0);
+}
+
+int	empty_map(char **map)
+{
+	int	i;
+
+	i = 0;
+	while (map[i])
+		i++;
+	return (i);
+}
+
+char	**read_and_check_nums(char *path)
+{
+	int		fd;
+	char	*text;
+	char	**map;
+
+	text = NULL;
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	text = read_file(fd);
+	close(fd);
+	if (!text)
+		return (NULL);
+	map = ft_split(text, '\n');
+	if (phasing(&text) == 0 || !map)
+		return (freemap(map, &text));
+	free(text);
+	return (map);
+}
+
+int	map_walls_check(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[0][i] && map[0][i] == '1')
+		i++;
+	if (map[0][i] != '\0')
+		return (0);
+	j = 0;
+	while (map[j] && map[j][0] == '1' && map[j][i - 1] == '1')
+		j++;
+	i = 0;
+	while (map[j - 1][i] && map[j - 1][i] == '1')
+		i++;
+	if (map[j - 1][i] != '\0')
+		return (0);
+	return (1);
+}
+
+int	collect_num(char **map)
+{
+	int	c_num;
+	int	x;
+	int	y;
+
+	c_num = 0;
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			if (map[y][x] == 'C' || map[y][x] == 'c')
+				c_num++;
+			x++;
+		}
+		y++;
+	}
+	return (c_num);
+}
+
+int	reach(char **map, t_point xy, char to_count, int firstcall)
+{
+	int	counter;
+
+	counter = 0;
+	if (map[xy.y][xy.x] == to_count)
+		counter++;
+	map[xy.y][xy.x] = 'F';
+	if (map[xy.y][xy.x + 1] != '1' && map[xy.y][xy.x + 1] != 'F')
+		counter += reach(map, (t_point){xy.x + 1, xy.y}, to_count, 0);
+	if (map[xy.y][xy.x - 1] != '1' && map[xy.y][xy.x - 1] != 'F')
+		counter += reach(map, (t_point){xy.x - 1, xy.y}, to_count, 0);
+	if (map[xy.y + 1][xy.x] != '1' && map[xy.y + 1][xy.x] != 'F')
+		counter += reach(map, (t_point){xy.x, xy.y + 1}, to_count, 0);
+	if (map[xy.y - 1][xy.x] != '1' && map[xy.y - 1][xy.x] != 'F')
+		counter += reach(map, (t_point){xy.x, xy.y - 1}, to_count, 0);
+	if (firstcall)
+		freemap(map, NULL);
+	return (counter);
+}
+
+t_point	xy_p(char **map)
+{
+	int		found;
+	t_point	xy_point;
+
+	found = 0;
+	xy_point.y = 0;
+	while (map[xy_point.y])
+	{
+		xy_point.x = 0;
+		while (map[xy_point.y][xy_point.x])
+		{
+			if (map[xy_point.y][xy_point.x] == 'P')
+			{
+				found = 1;
+				break ;
+			}
+			xy_point.x++;
+		}
+		if (found)
+			break ;
+		xy_point.y++;
+	}
+	return (xy_point);
+}
+
+char	**cpymap(char **map)
+{
+	int		i;
+	int		j;
+	char	**cmap;
+
+	i = 0;
+	while (map[i])
+		i++;
+	j = 0;
+	while (map[0][j])
+		j++;
+	cmap = malloc(sizeof(char *) * (i + 1));
+	cmap[i] = NULL;
+	i = 0;
+	while (map[i])
+	{
+		cmap[i] = malloc(j + 1);
+		if (!cmap[i])
+			return (freemap(cmap, NULL));
+		j = 0;
+		while (map[i][j++])
+			cmap[i][j - 1] = map[i][j - 1];
+		cmap[i][j - 1] = '\0';
+		i++;
+	}
+	return (cmap);
+}
+
+int	map_check_rect(char **map)
+{
+	int	i;
+	int	l;
+	int	old_l;
+
+	i = 0;
+	old_l = 0;
+	while (map[i])
+	{
+		l = 0;
+		while (map[i][l])
+			l++;
+		if (old_l != 0 && old_l != l)
+		{
+			write (1, "ERROR\nnot a rectangle map\n", 26);
+			return (0);
+		}
+		old_l = l;
+		i++;
+	}
+	return (1);
+}
+
+int	check_extention(char *path, char const *ber)
+{
+	int	i;
+	int	num_match;
+
+	i = 0;
+	while (path[i])
+		i++;
+	i -= 4;
+	num_match = 0;
+	while (path[i])
+	{
+		if (path[i] == ber[num_match])
+			num_match++;
+		i++;
+	}
+	if (num_match != 4)
+		return (0);
+	return (1);
+}
+
+char	**map_manager(char *path)
+{
+	char	**map;
+
+	map = read_and_check_nums(path);
+	if (!map)
+		return (NULL);
+	if (map_walls_check(map) == 0 || map_check_rect(map) == 0)
+		write (1, "Error\ncheck walls or check rectangle wrong\n", 43);
+	else if (reach(cpymap(map), xy_p(map), 'E', 1) != 1
+		|| reach(cpymap(map), xy_p(map), 'C', 1) != collect_num(map))
+		write (1, "Error\ncouldn't reach all C and E\n", 33);
+	else
+		return (map);
+	freemap(map, NULL);
+	return (NULL);
+}
+/*
+int main(int ac, char **av)
+{
+	// int		i;///////////
+	// char	*path = "maplib/map.ber";//////////
+	char	**map;
+
+	if (ac != 2 || check_extention(av[1], ".ber") == 0)
+	{
+		printf("bad extention\n");
+		write (1, "Error\nmust take path as argument\n", 33);
+		return (0);
+	}
+	map = map_manager(av[1]);
+	// map = read_and_check_nums(av[1]);
+	// printf("check walls : %d\n", map_walls_check(map));///////
+	if (map)
+		freemap(map, NULL);
+    return (0);
+}
+*/
---
+++ core/so_long/work6/render/render2.c
@@ -0,0 +1,223 @@
+/* ************************************************************************** */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   render1.c										  :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: melshata <melshata@student.42.fr>		  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2026/01/08 17:29:32 by melshata		  #+#	#+#			 */
+/*   Updated: 2026/01/10 11:43:18 by melshata		 ###   ########.fr	   */
+/*																			*/
+/* ************************************************************************** */
+
+#include "../so_long.h"
+/*
+int	handle_exit(t_game *game)
+{
+	int	i;
+
+	free(game->mlx);
+	free(game->win);
+	i = 0;
+	while (game->map_base[i])
+		free(game->map_base[i++]);
+	i = 0;
+	while (game->map_grid[i])
+		free(game->map_grid[i++]);
+	free(game->map_grid);
+	free(game->wall_img);
+	free(game->floor_img);
+	free(game->player_img);
+	free(game->collect_img);
+	free(game->exit_img);
+	exit(0);
+	return (0);
+}
+*/
+void	print_map(t_game *game)
+{
+	int	i;
+
+	printf("printing maps\n");
+	i = 0;
+	while (game->map_base[i])
+		printf("%s\n", game->map_base[i++]);
+	i = 0;
+	while (game->map_grid[i])
+		printf("%s\n", game->map_grid[i++]);
+	printf("done printing maps\n");
+}
+
+int handle_exit(t_game *game, int exit_code)
+{
+    if (game->wall_img)
+        mlx_destroy_image(game->mlx, game->wall_img);
+    if (game->floor_img)
+        mlx_destroy_image(game->mlx, game->floor_img);
+    if (game->player_img)
+        mlx_destroy_image(game->mlx, game->player_img);
+    if (game->collect_img)
+        mlx_destroy_image(game->mlx, game->collect_img);
+    if (game->exit_img)
+        mlx_destroy_image(game->mlx, game->exit_img);
+    if (game->win)
+        mlx_destroy_window(game->mlx, game->win);
+    if (game->mlx)
+    {
+        mlx_destroy_display(game->mlx);
+        free(game->mlx);
+    }
+    freemap(game->map_base, NULL);
+    freemap(game->map_grid, NULL);
+    exit(exit_code);
+    return (0);
+}
+
+void	*load_texture(t_game *game, char *path)
+{
+	int w, h;
+	void *img = mlx_xpm_file_to_image(game->mlx, path, &w, &h);
+	if (!img) {
+		write(1, "Error loading ", 14);
+		write(1, path, 10);
+		handle_exit(game, 1);
+	}
+	return (img);
+}
+
+void	set_xy(t_game *game, char **map)
+{
+	game->collected = 0;
+	game->player_x = xy_p(map).x;
+	game->player_y = xy_p(map).y;
+	game->map_base[game->player_y][game->player_x] = '0';
+	game->map_y = 0;
+	while (game->map_grid && game->map_grid[game->map_y])
+		game->map_y++;
+	game->map_x = 0;
+	while (	game->map_grid &&
+			game->map_grid[0] &&
+			game->map_grid[0][game->map_x])
+		game->map_x++;
+}
+
+int	render_frame(t_game *game)
+{
+	t_point	xy;
+
+	xy.y = 0;
+	while (xy.y < game->map_y) 
+	{
+		xy.x = 0;
+		while (xy.x < game->map_x) 
+		{
+			void *img = game->floor_img; 
+			if (game->map_grid[xy.y][xy.x] == '1')
+				img = game->wall_img;
+			else if (game->map_grid[xy.y][xy.x] == 'P')
+				img = game->player_img;
+			else if (game->map_grid[xy.y][xy.x] == 'C')
+				img = game->collect_img;
+			else if (game->map_grid[xy.y][xy.x] == 'E')
+				img = game->exit_img;
+			mlx_put_image_to_window(game->mlx, game->win, img,
+									xy.x * IMG_SIZE, xy.y * IMG_SIZE);
+			xy.x++;
+		}
+		xy.y++;
+	}
+	return (0);
+}
+
+void move_player(t_game *game, int new_x, int new_y)
+{
+	char	new_xy_char;
+
+	if (game->map_grid[new_y][new_x] == '1')
+		return;
+	new_xy_char = game->map_grid[new_y][new_x];
+	printf("you got: %d, all: %d\n", game->collected, collect_num(game->map_base));
+	if (new_xy_char == 'E' && game->collected == collect_num(game->map_base))
+		handle_exit(game, 0);
+	else if (new_xy_char == 'C')
+	{
+		game->map_base[new_y][new_x] = 'c';
+		game->collected++;
+	}
+	game->map_grid[game->player_y][game->player_x] =
+		game->map_base[game->player_y][game->player_x];
+	game->map_grid[new_y][new_x] = 'P';
+	game->player_x = new_x;
+	game->player_y = new_y;
+	render_frame(game);
+}
+
+int handle_keypress(int keysym, t_game *game)
+{
+	if (keysym == KEY_ESC)
+		handle_exit(game, 0);
+	else if (keysym == KEY_UP)
+		move_player(game, game->player_x, game->player_y - 1);
+	else if (keysym == KEY_DOWN)
+		move_player(game, game->player_x, game->player_y + 1);
+	else if (keysym == KEY_LEFT)
+		move_player(game, game->player_x - 1, game->player_y);
+	else if (keysym == KEY_RIGHT)
+		move_player(game, game->player_x + 1, game->player_y);
+	return (0);
+}
+
+void	load_photos(t_game *game)
+{
+	game->wall_img = load_texture(game, "photos/wall.xpm");
+	game->floor_img = load_texture(game, "photos/floor.xpm");
+	game->player_img = load_texture(game, "photos/cat_right.xpm");
+	game->collect_img = load_texture(game, "photos/collect.xpm");
+	game->exit_img = load_texture(game, "photos/exit.xpm");
+}
+
+void	initiate_game(t_game *game)
+{
+	game->mlx = NULL;
+	game->win = NULL;
+	game->map_base = NULL;
+	game->map_grid = NULL;
+	game->wall_img = NULL;
+	game->floor_img = NULL;
+	game->player_img = NULL;
+	game->collect_img = NULL;
+	game->exit_img = NULL;
+	game->player_x = 0;
+	game->player_y = 0;
+	game->map_x = 0;
+	game->map_y = 0;
+	game->collected = 0;
+}
+
+int main(int ac, char **av)
+{
+	t_game  game;
+
+	if (ac != 2)
+		return (0);
+	initiate_game(&game);
+	game.map_base = map_manager(av[1]);
+	if (!game.map_base)
+		handle_exit(&game, 1);
+	game.map_grid = cpymap(game.map_base);
+	if (!game.map_grid)
+		handle_exit(&game, 1);
+	// print_map(&game);
+	set_xy(&game, game.map_base);
+	game.mlx = mlx_init();
+	if (!game.mlx)
+		return (1);
+	load_photos(&game);
+	game.win = mlx_new_window(	game.mlx, game.map_x * IMG_SIZE,
+								game.map_y * IMG_SIZE, "So Long Move");
+	render_frame(&game);
+	mlx_key_hook(game.win, handle_keypress, &game);
+	mlx_hook(game.win, 17, 0, handle_exit, &game);
+	mlx_loop(game.mlx);
+	return (0);
+}
